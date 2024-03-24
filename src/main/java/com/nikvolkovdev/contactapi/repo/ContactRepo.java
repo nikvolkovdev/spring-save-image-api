6f481e17be727ad4dde1fcaaa027f70fcf7a17f9
nikvolkovdev <nikvolkovmsk@gmail.com>
2024-03-24 20:24:07 +0500

    first commit

---
+++ src/main/java/com/nikvolkovdev/contactapi/repo/ContactRepo.java
@@ -0,0 +1,12 @@
+package com.nikvolkovdev.contactapi.repo;
+
+import com.nikvolkovdev.contactapi.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+}
